8afa3a702dd6d79aacf733628b874cb9fc81a58b
Manishyadav514 <60292480+Manishyadav514@users.noreply.github.com>
2020-09-17 01:07:34 +0530

    For Loop to make the option dynamic and modular.

--- safarnama/views.py
+++ safarnama/views.py
@@ -9,5 +9,19 @@ def index(request):
     dest1.desc="The city of fun"
     dest1.price=7000
     dest1.img="destination_1.jpg"
+
+    dest2= Destination()
+    dest2.name="UTTRAKHAND"
+    dest2.desc="THe GateWay to heaven"
+    dest2.price=8000
+    dest2.img="destination_2.jpg"
+
+    dest3= Destination()
+    dest3.name="Delhi"
+    dest3.desc="Capital"
+    dest3.price=9000
+    dest3.img="destination_3.jpg"
+
+    dests=[dest1, dest2, dest3]
     
-    return render(request, "index.html", {'dest1': dest1 })
+    return render(request, "index.html", {'dests': dests })
