8d0b75e3f6e4fb02ad4f759ce04ad7b1833efa45
Manishyadav514 <60292480+Manishyadav514@users.noreply.github.com>
2020-09-16 21:38:11 +0530

    Manking class of the destination

--- safarnama/views.py
+++ safarnama/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
-
+# To change handle the dstination from here
 def index(request):
-    return render(request, "index.html")
+    dest1= Destination()
+    dest1.name="GOA"
+    dest1.desc="The city of fun"
+    dest1.price=7000
+    dest1.img="destination_1.jpg"
+    
+    return render(request, "index.html", {'dest1': dest1 })
