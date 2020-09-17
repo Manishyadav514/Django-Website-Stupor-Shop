81bd67d08519dfae4fe24929476381087ce3ec3c
Manishyadav514 <60292480+Manishyadav514@users.noreply.github.com>
2020-09-17 20:23:34 +0530

    Postgres and PgAdmin Setup.

--- Flopkart/settings.py
+++ Flopkart/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'safarnama.apps.SafarnamaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,12 +74,21 @@ WSGI_APPLICATION = 'Flopkart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Flopkart',
+        'USER':'postgres',
+        'PASSWORD': '1234',
+        'HOST':'localhost',
+        'PORT':''
     }
 }
+
+
+
 
 
 # Password validation
