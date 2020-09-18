3dcd751b51ab401a0d57c0f97b6419d0a5d87f7c
Manishyadav514 <60292480+Manishyadav514@users.noreply.github.com>
2020-09-18 20:12:29 +0530

    User Login

--- account/views.py
+++ account/views.py
@@ -1,7 +1,54 @@
-from django.shortcuts import render
-  
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 # Create your views here.
 # To change handle the dstination from here
+
+
+
+def login(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            messages.info(request, 'invalid credentials')
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html')
+
+
+
+
 def register(request):
-    return render(request,'register.html')
+
+    if request.method=='POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+
+        if password1==password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,'Username taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'email taken')
+                return redirect('register')
+            else:
+                user=User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+                user.save();
+                print('user created')
+                return redirct ('login')
+        else:
+            messages.info(request,'Passwords do not match')
+            return redirect('register')
+        return redirect( '/')
+    else:
+        return render(request,'register.html')
