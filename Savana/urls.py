1840d3b54659d64bfd2e5ac07ff84eea5b0c5bff
Manishyadav514 <60292480+Manishyadav514@users.noreply.github.com>
2020-10-09 23:31:36 +0530

    New
    Signed-off-by: Manishyadav514 <60292480+Manishyadav514@users.noreply.github.com>

--- Savana/urls.py
+++ Savana/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('SavanaApp.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('account/', include('account.urls'))
 ]
